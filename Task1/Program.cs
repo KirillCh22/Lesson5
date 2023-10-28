8832ae4206298c59377b85c7587a7d289af2c627
Kirill <147234461+KirillCh22@users.noreply.github.com>
2023-10-28 15:14:43 +0300

    Lesson 5

---
+++ Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿//int[] array = new int[8];
+int[,] array = GenerateArray2D();
+PrintArray(array);
+Console.WriteLine("_________________________________________________________");
+Square(array);
+PrintArray(array);
+Console.WriteLine("_________________________________________________________");
+
+
+int[,] GenerateArray2D()
+{
+    int[,] array = new int[6, 8];
+
+    for(int i=0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+
+
+void PrintArray(int[,] array)
+{
+    for(int i=0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void Square(int[,] array)
+{
+    for(int i=0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+             if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+}
+
